54b47aff17d6d82d1d07a76bd76c0774f51171af
Emanuekl Gjini <emanuel_gjini@yahoo.com>
2021-01-25 15:21:12 +0100

    Test conf

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -57,7 +57,7 @@ class App extends React.Component {
     const token = window.localStorage.getItem('jwt');
     if (token && !window.webSocket){
 
-      const webSocket = new WebSocket('ws://62.171.169.144:9000?token=' + token);
+      const webSocket = new WebSocket('ws://channels.critias.io?token=' + token);
       webSocket.onopen = e => {
         console.log('open', e);
       }
